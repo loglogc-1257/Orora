e90bf3b1b365da1754b9c36d4da6c3971361769f
loglogc-1257 <fadonougbosebastien7@gmail.com>
2025-02-02 13:35:56 +0100

    Update contact.js

--- commands/contact.js
+++ commands/contact.js
@@ -1,17 +1,1 @@
-const axios = require('axios');
 
-module.exports = {
-  name: 'contact',
-  description: 'Get the developer\'s contact information',
-  author: 'Aljur Pogoy',
-  async execute(senderId, args, pageAccessToken, sendMessage) {
-    // Phone number and email address of the developer
-    const phoneNumber = '09120121191'; // Replace with the actual phone number
-    const emailAddress = 'Korisawaumuzaki@gmail.com'; // Replace with the actual email address
-
-    sendMessage(senderId, {
-      text: `You can reach the developer at:\n\nPhone: ${phoneNumber}\nEmail: ${emailAddress}`
-    }, pageAccessToken);
-  }
-};
-                
